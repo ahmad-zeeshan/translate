07cc8859f0952efe3298173c5b092692351a8086
ahmad-zeeshan <zeeshanahmad-25@hotmail.com>
2019-08-20 21:35:34 +0300

    Pulling from multiple contexts using consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   /*
@@ -13,13 +14,21 @@ as a 'child'; that child would be called with whatever value inside our 'pipe'.
 */
   static contextType = LanguageContext;
 
-  render() {
+  renderButton(color) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {value => (value === "english" ? "Submit" : "Voorleggen")}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
