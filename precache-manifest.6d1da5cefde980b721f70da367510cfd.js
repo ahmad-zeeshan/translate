971476cdcf2105f9f9552288ac4199a42a7c860a
ahmad-zeeshan <zeeshanahmad-25@hotmail.com>
2019-08-20 21:43:06 +0300

    Updates

---
+++ precache-manifest.6d1da5cefde980b721f70da367510cfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efec537e4609103794f0d344ae5b754f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d8a6f6cdb038ac25c4",
+    "url": "/static/js/2.f6fbc022.chunk.js"
+  },
+  {
+    "revision": "cf093f7eae72936ad32c",
+    "url": "/static/js/main.58b98b5f.chunk.js"
+  },
+  {
+    "revision": "5583a1c3abd780fb8dc0",
+    "url": "/static/js/runtime~main.3afc4f72.js"
+  }
+]);
